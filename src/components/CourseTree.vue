eda5c8c5d17634fd1cc8c0251a4e2b78667231cf
Ali Raza <alir.raza@mail.utoronto.ca>
2020-03-27 03:53:03 -0400

    graphql added for all queries

--- src/components/CourseTree.vue
+++ src/components/CourseTree.vue
@@ -15,7 +15,7 @@
         class="hidden-sm-and-down"
         v-model="model"
         :items="getCourse"
-        :loading="isLoading"
+        :loading="$apollo.loading"
         :search-input.sync="search"
         item-text="name"
         no-filter
@@ -37,7 +37,7 @@
           >
             {{ item.code.charAt(0) + item.code.charAt(1) }}
           </v-list-item-avatar>
-          <v-list-item-content>
+          <v-list-item-content v-if="item">
             <v-list-item-title v-text="item.name"></v-list-item-title>
             <v-list-item-subtitle v-text="item.code"></v-list-item-subtitle>
           </v-list-item-content>
@@ -54,7 +54,6 @@
           ></v-progress-circular>
           <v-treeview
             v-else
-            v-model="selection"
             :items="tree"
             activatable
             :active.sync="active"
@@ -71,18 +70,7 @@
           class="pt-6 mx-auto"
           flat
           max-width="900"
-          v-if="!selected && !selected_data && !loading"
-        >
-          <div class="title grey--text text--lighten-1 font-weight-light">
-            Select a Course for more info
-          </div>
-        </v-card>
-        <v-card
-          :key="1"
-          class="pt-6 mx-auto"
-          flat
-          max-width="900"
-          v-else-if="selected && loadingCourse"
+          v-if="selected && $apollo.loading"
         >
           <div class="title grey--text text--lighten-1 font-weight-light">
             <v-progress-circular
@@ -93,62 +81,97 @@
           </div>
         </v-card>
         <v-card v-else :key="1" class="pt-6 mx-auto" flat max-width="900">
-          <v-card-text v-if="selected_data">
+          <v-card-text v-if="getData && getData.length > 0">
             <h3 class="headline mb-2">
-              <a :href="`/view?course=${selected_data.code}`" target="_blank"
-                >{{ selected_data.code }} - {{ selected_data.name }}</a
+              <a :href="`/view?course=${getData[0].code}`" target="_blank"
+                >{{ getData[0].code }} - {{ getData[0].name }}</a
               >
             </h3>
           </v-card-text>
           <v-divider></v-divider>
-          <v-row v-if="selected_data" class="text-left" tag="v-card-text">
-            <v-col class="text-right mr-4 mb-4" tag="strong" cols="2"
+          <v-row
+            v-if="getData && getData.length > 0"
+            class="text-left"
+            tag="v-card-text"
+          >
+            <v-col class="text-left mr-1 mb-2" tag="strong" cols="2"
               >Description:</v-col
             >
-            <v-col>{{ selected_data.description }}</v-col>
-            <!-- <v-col class="text-right mr-2 mb-4" tag="strong" cols="3">description:</v-col>
-              <v-col>{{ selected_data.description }}</v-col> -->
+            <v-col>{{ getData[0].description }}</v-col>
             <!-- <v-col class="text-right mr-4 mb-2" tag="strong" cols="5">Website:</v-col> -->
             <!-- <v-col>
                 <a :href="`//${selected.website}`" target="_blank">{{ selected.website }}</a>
               </v-col> -->
           </v-row>
-          <v-row class="text-left" tag="v-card-text" v-if="selected_data">
-            <v-col class="text-right mr-4 mb-2" tag="strong" cols="2"
+          <v-row
+            class="text-left"
+            tag="v-card-text"
+            v-if="getData && getData.length > 0"
+          >
+            <v-col class="text-left mr-1 mb-2" tag="strong" cols="2"
               >Campus:</v-col
             >
-            <v-col>{{ selected_data.campus }}</v-col>
+            <v-col>{{ getData[0].campus }}</v-col>
+          </v-row>
+          <v-row
+            class="text-left"
+            tag="v-card-text"
+            v-if="
+              getData && getData.length > 0 && getData[0].exclusions.length > 0
+            "
+          >
+            <v-col
+              class="text-left mr-1 mb-2"
+              tag="strong"
+              cols="2"
+              v-if="
+                getData &&
+                  getData.length > 0 &&
+                  getData[0].exclusions.length > 0
+              "
+              >Exclusions:</v-col
+            >
+            <v-col>{{ getData[0].exclusions }}</v-col>
+          </v-row>
+          <v-row
+            class="text-left"
+            tag="v-card-text"
+            v-if="
+              getData &&
+                getData.length > 0 &&
+                getData[0].prerequisites.length > 0
+            "
+          >
+            <v-col class="text-left mr-1 mb-2" tag="strong" cols="2"
+              >Prerequisites:</v-col
+            >
+            <v-col>{{ getData[0].prerequisites }}</v-col>
+          </v-row>
+          <v-row
+            class="text-left"
+            tag="v-card-text"
+            v-if="getData && getData.length > 0 && getData[0].required_for"
+          >
+            <v-col
+              class="text-left mr-1 mb-2"
+              tag="strong"
+              cols="2"
+              v-if="
+                getData &&
+                  getData.length > 0 &&
+                  getData[0].required_for.length > 0
+              "
+              >Necessary for:</v-col
+            >
+            <v-col
+              ><div v-for="item of getData[0].required_for" v-bind:key="item">
+                <a :href="`/tree?course=${item}`">{{ item }}</a>
+              </div></v-col
+            >
           </v-row>
         </v-card>
       </v-col>
     </v-row>
-    <!-- <v-row>
-      <v-col class="d-flex text-center">
-        <v-card :key="1" class="pt-6 mx-auto" flat>
-          <div v-for="course in getCourse" :key="course.name">
-            {{ course.name + " " + course.code }}
-          </div>
-          <ApolloQuery
-            v-if="search"
-            :query="require('../graphql/getCourse.gql')"
-            :variables="{ search }"
-          >
-            <template v-slot="{ result: { loading, error, data } }">
-              <div v-if="data">
-                <div v-for="course in data.getCourse" :key="course.code">
-                  {{ "Name: " + course.name + " Code: " + course.code }}
-                </div>
-              </div>
-            </template>
-          </ApolloQuery>
-          <div>
-            <div v-for="course in getCourse" :key="course.code">
-              {{ "Name: " + course.name + " Code: " + course.code }}
-            </div>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row> -->
   </v-card>
 </template>
 
@@ -172,25 +195,42 @@ export default {
         };
       },
       skip() {
-        return !this.search || this.search.length < 3;
+        return !this.search ? true : this.search.length < 3 ? true : false;
+      }
+    },
+    getData: {
+      query: gql`
+        query getCourse($search: String!) {
+          getData: getCourse(search: $search) {
+            code
+            name
+            description
+            campus
+            prerequisites
+            prerequisites_array
+            required_for
+            exclusions
+          }
+        }
+      `,
+      variables() {
+        return {
+          search: this.dosearch
+        };
+      },
+      skip() {
+        return !this.dosearch;
       }
     }
   },
   data: () => ({
     currentCourse: null,
     active: [],
-    selected_data: null,
-    items: [],
     loading: false,
-    loadingCourse: false,
     tree: [],
-    open: [],
-    selection: [],
-    descriptionLimit: 60,
-    entries: [],
-    isLoading: false,
     model: null,
-    search: null
+    search: null,
+    dosearch: null
   }),
   computed: {
     selected() {
@@ -208,70 +248,24 @@ export default {
           value: this.model[key] || "n/a"
         };
       });
-    },
-    searchitems() {
-      return this.entries.map(entry => {
-        const query =
-          entry.name.length > this.descriptionLimit
-            ? entry.code +
-              " : " +
-              entry.name.slice(0, this.descriptionLimit) +
-              "..."
-            : entry.code + " : " + entry.name;
-        return Object.assign({}, entry, { query });
-      });
     }
   },
   watch: {
     selected(newValue) {
-      newValue ? this.onUpdate(newValue) : null;
+      newValue ? (this.dosearch = newValue) : null;
     },
     $route: "fetchData",
-    search(val) {
-      // Items have already been loaded
-      if (this.model && val.length > 10) return;
-      if (val.length < 4) return;
-      // Items have already been requested
-      if (this.isLoading) return;
-      this.isLoading = true;
-      // Lazily load input items
-      axios
-        .get(`http://localhost:8080/api/search_queries?query=` + val)
-        .then(response => {
-          // console.log(response.data)
-          this.entries = response.data.result;
-        })
-        .catch(err => {
-          console.log(err);
-        })
-        .finally(() => (this.isLoading = false));
-    },
     model: function(val) {
-      this.fetchData(val.code);
-    }
-  },
-  mounted: function() {
-    if (this.$route.query.course) {
-      this.course = this.$route.query.course;
+      if (val) {
+        this.dosearch = val.code;
+        this.fetchData(val.code);
+      }
     }
   },
   created() {
     this.fetchData(this.$route.query.course);
   },
   methods: {
-    text: item => item.code + " — " + item.name,
-    onUpdate(selection) {
-      this.loadingCourse = true;
-      axios
-        .get(`http://localhost:8080/api/search?course=` + selection)
-        .then(response => {
-          this.loadingCourse = false;
-          this.selected_data = response.data;
-        })
-        .catch(e => {
-          this.errors = e.toString();
-        });
-    },
     fetchData(val) {
       this.loading = true;
       this.currentCourse = null;
@@ -279,10 +273,9 @@ export default {
         .get(`http://localhost:8080/api/tree?course=` + val)
         .then(response => {
           this.loading = false;
-          this.currentCourse = val;
+          this.currentCourse = val.toUpperCase();
+          this.dosearch = val.toUpperCase();
           this.search = "";
-          this.items = [];
-          this.onUpdate(val);
           this.tree = [response.data];
         })
         .catch(e => {
